6b16b35cf6e2f1669dcc1ea5fe2b604c62826091
anissamh <an.infmhiri@gmail.com>
2024-10-02 08:58:07 +0200

    commited

--- src/main/java/lavage/PileDAssiettes.java
+++ src/main/java/lavage/PileDAssiettes.java
@@ -16,11 +16,20 @@ class PileDAssiettes {
 		return (myList.size() >= MAX);
 	}
 
-	public void push(Assiette assiette) throws InterruptedException {
+	synchronized	public void push(Assiette assiette) throws InterruptedException {
+		while ((isFull())) {
+
+			wait();
+		}
 		myList.add(assiette);
 		System.out.printf("la pile contient %d assiettes%n", myList.size());
+		//ici les laveurs pushen tque les assiettes sont lavées mais ne notifient personne c pour cela que l'essuyeur n arrive pas au debut à acceder à l'assiette
+		notifyAll(); // Notifier que la pile est  pleine
+
 	}
 
+	//synchronized pour eviter que ça crache tout est sychro car on a +rurs thread du coup bcp de choses utilisent
+	//pour sortr d un wait il faut notifier le thread
 	synchronized public Assiette pop() throws InterruptedException {
 		// Attendre que la pile ne soit pas vide
 		while (isEmpty()) {
